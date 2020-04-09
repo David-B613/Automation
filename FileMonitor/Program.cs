4f0db89daad5cba02a22829a01dc02fc82243795
David-B613 <60449471+David-B613@users.noreply.github.com>
2020-04-09 17:01:38 -0400

    Automation Projects

---
+++ FileMonitor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace FileMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher FO = new FileSystemWatcher(@"C:\"); // The location of where you will monitor this can be changed to anything 
+            FO.EnableRaisingEvents = true;
+            FO.IncludeSubdirectories = true;
+
+
+            FO.Changed += FileChanged;     // Method for when something changes in the location 
+
+            FO.Created += FileCreated;     // Method for when something is created in the location
+
+            FO.Deleted += FileDeleted;     // Method for when something changes in the location ** This could mean the file was removed from the folder into a new folder 
+                                           // Something I can come back make better.**
+
+            FO.Renamed += FileRenamed;     // Method for when a file name is changed in the location
+
+            Console.Read(); // This is to stop the program from running 
+
+
+        }
+
+        static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("File: {0} renamed to {1} at time: {2}", e.OldName, e.Name, DateTime.Now.ToLocalTime());
+        }
+
+        static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: {0} deleted at time: {1}", e.Name, DateTime.Now.ToLocalTime());
+        }
+
+        static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: {0} Created at time {1},", e.Name, DateTime.Now.ToLocalTime());
+
+        }
+
+        static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File {0} Changed at time {1}", e.Name, DateTime.Now.ToLocalTime());
+        }
+    }
+}
---
+++ FileOrganizer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Test 
+{
+    public static void Main() 
+    {
+        string path = @"C:\Users\baber\Desktop\MyTest123.txt";
+
+        // Create the file, or overwrite if the file exists.
+        using (FileStream fs = File.Create(path, 1024)) 
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+            // Add some information to the file.
+            fs.Write(info, 0, info.Length);
+        }
+
+        // Open the stream and read it back.
+        using (StreamReader sr = File.OpenText(path)) 
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null) 
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
